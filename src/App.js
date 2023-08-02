a64119941f6dbe78fe2b29ebe6a32328cb9fd353
Atul Panchal <83485794+Atul-Panchal@users.noreply.github.com>
2023-08-02 14:26:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Map from './components/Map';
-import axios from 'axios';
+import React, { useState } from 'react';
+import Map from '.components/Map';
 
 const App = () => {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
+  const [inputLocation, setInputLocation] = useState('');
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLocation({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }, []);
+  const handleInputChange = (event) => {
+    setInputLocation(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const [lat, lng] = inputLocation.split(',').map((coord) => parseFloat(coord.trim()));
+
+    setLocation({ lat, lng });
+  };
 
   return (
     <div>
-      <h1>My Location Map</h1>
+      <h1>Geo Location Map</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={inputLocation}
+          onChange={handleInputChange}
+          placeholder="Enter latitude and longitude"
+        />
+        <button type="submit">Show Location</button>
+      </form>
       {location.lat !== 0 && location.lng !== 0 ? (
         <Map location={location} />
       ) : (
-        <p>Loading...</p>
+        <p>Please enter a valid latitude and longitude.</p>
       )}
     </div>
   );
